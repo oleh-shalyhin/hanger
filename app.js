1031fb447ee483dfc5b4f1f3c094144a37e2cd7f
Oleh Shalyhin <cross14ek@yandex.ru>
2017-02-27 01:27:17 +0200

    added favicon

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
+var favicon = require('serve-favicon');
+var path = require('path');
 
 var app = express();
 
+app.use(favicon(path.join(__dirname, '/public/images/favicon.ico')));
 app.engine('handlebars', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', 'handlebars');
 
