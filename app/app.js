6dbf07c74abcf99a8b060327909877d8983feac9
Oleh Shalyhin <cross14ek@yandex.ru>
2017-02-27 01:05:36 +0200

    changed view engine to express-handlebars and set it up

--- app/app.js
+++ app/app.js
@@ -1,5 +1,10 @@
 var express = require('express');
+var exphbs = require('express-handlebars');
+
 var app = express();
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
