6d8da716e16dd5a23d8dcad1814600d9d2fc4cde
Oleh Shalyhin <cross14ek@yandex.ru>
2017-03-21 01:20:09 +0200

    #10 - added basic routing

--- app.js
+++ app.js
@@ -10,8 +10,66 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Home page');
 });
+
+app.post('/signup', function(req, res) {
+
+});
+
+app.post('/login', function(req, res) {
+
+});
+
+
+app.get('/find', function(req, res) {
+	res.send('find');
+});
+
+app.route('/friends')
+	.get(function(req, res) {
+		res.send('friends');
+	})
+	.post(function(req, res) {
+
+	})
+	.delete(function(req, res) {
+
+	});
+
+app.route('/albums')
+	.get(function(req, res) {
+		res.send('albums');
+	})
+	.post(function(req, res) {
+
+	})
+	.delete(function(req, res) {
+
+	});
+
+app.route('/albums/:albumId')
+	.get(function(req, res) {
+		res.send('albums/:albumId');
+	})
+	.post(function(req, res) {
+
+	})
+	.delete(function(req, res) {
+
+	});
+
+app.post('/albums/:albumId/cover', function(req, res) {
+
+});
+
+app.route('/:userId')
+	.get(function(req, res) {
+		res.send(':userId');
+	})
+	.post(function(req, res) {
+
+	});
 
 app.listen(3000, function () {
   console.log('App is listening on port 3000!');
